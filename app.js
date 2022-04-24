481ed70971f8e156022f715fd25b99c65b3f6737
charuzain <charuabhizain@gmail.com>
2022-04-24 18:54:08 -0400

    Add fetch request on click eventlistener on buttons

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 const url = "https://swapi.dev/api/";
 
+// const getData = (link)=>{
+//   fetch(link)
+//     .then(data=>data.json())
+//     .then(res=>console.log(res));
+// };
+
+
+// const fetchData = (e)=>{
+//   // console.log(e.target);
+//   const btnClicked = e.target;
+//   // console.log(btnClicked);
+//   // console.log(btnClicked.link);
+//   getData(btnClicked.link);
+// };
+
 
 
 window.addEventListener('DOMContentLoaded',(event)=>{
-  console.log("Dom Loaded");
+  // console.log("Dom Loaded");
   const nav = document.querySelector('#btn-container');
-  console.log(nav);
+  // console.log(nav);
   fetch(url).then((data)=>data.json()).then(res=>{
     console.log(res);
-    // nav.innerHTML = '';
     for (let property in res) {
-      console.log(property);
       const btn = document.createElement('button');
-      btn.textContent = `${property}`;
-      console.log(btn);
-      btn.classList.add("nav-btn");
+      btn.textContent = property;
 
+      btn.classList.add("nav-btn");
+      btn.link = res[property];
       nav.append(btn);
+
+
+      /* Event listener to button */
+      btn.addEventListener('click', (e)=>{
+        const btnClicked = e.target;
+        fetch(btnClicked.link)
+          .then(data=>data.json())
+          .then(res=>{
+            console.log(res.results);
+
+          });
+
+      });
+
 
     }
   });
