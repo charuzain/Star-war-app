cf692ec1a1361322f4125364c10e6d2c63d2c252
charuzain <charuabhizain@gmail.com>
2022-04-24 17:02:02 -0400

    Dynamically added buttons to DOM

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const url = "https://swapi.dev/api/";
 
+
+
 window.addEventListener('DOMContentLoaded',(event)=>{
   console.log("dom loaded");
+  const h2 = document.querySelector('#nav');
+  console.log(h2);
   fetch(url).then((data)=>data.json()).then(res=>{
     console.log(res);
+    // h2.innerHTML = '';
     for (let property in res) {
       console.log(property);
+      const btn = document.createElement('button');
+      btn.textContent = `${property}`;
+      console.log(btn);
+
+      console.log(btn);
+      h2.append(btn);
+
     }
   });
 });
